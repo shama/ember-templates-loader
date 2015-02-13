ae71785e7b6db15dd72f910eba70e25a832c361e
Kyle Robinson Young <kyle@dontkry.com>
2015-02-13 10:28:16 -0800

    Wrap template in Ember.HTMLBars.template()

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var precompile = require('ember/ember-template-compiler').precompile;
 module.exports = function(source) {
   this.cacheable && this.cacheable();
-  return 'module.exports = ' + precompile(source) + ';';
+  return 'module.exports = Ember.HTMLBars.template(' + precompile(source) + ');';
 }
 module.exports.seperable = true;
