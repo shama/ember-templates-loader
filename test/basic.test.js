700ac9097a6a67dbf91c281f094394531bcebec7
Kyle Robinson Young <kyle@dontkry.com>
2017-08-31 15:27:13 -0700

    Update deps and compiler to 2.14.1

--- test/basic.test.js
+++ test/basic.test.js
@@ -2,7 +2,7 @@ var should = require('should');
 
 describe('basic', function() {
   it('should compile hbs', function() {
-    var hbs = require('raw!../!./fixtures/basic.hbs');
+    var hbs = require('raw-loader!../!./fixtures/basic.hbs');
     hbs.should.containEql('module.exports = ');
     hbs.should.containEql('dom.createTextNode("hi");');
   });
